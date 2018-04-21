1439f8f5bdf22764d2d8a32787628a651825c349
yangboz <youngwelle@gmail.com>
2018-04-21 18:19:46 +0800

    CHANGED:modification for mini-app;

--- RandD/weixin-app/utils/config.js
+++ RandD/weixin-app/utils/config.js
@@ -13,9 +13,9 @@
 
 //配置域名,域名只修改此处。
 //如果wordpress没有安装在网站根目录请加上目录路径,例如："www.watch-life.net/blog"
-var DOMAIN = "www.watch-life.net";
+var DOMAIN = "knighter.cn";
 var MINAPPTYPE="0";//小程序的类型，如果是企业小程序请填：0 ，如果是个人小程序请填：1
-var WEBSITENAME="守望轩"; //网站名称
+var WEBSITENAME="骑士的勇敢"; //网站名称
 var ABOUTID = 1136; //wordpress网站"页面"的id,注意这个"页面"是wordpress的"页面"，不是"文章"
 var PAGECOUNT='10'; //每页文章数目
 var CATEGORIESID='all'  //专题页显示全部的分类
@@ -24,15 +24,15 @@ var INDEXLISTTYPE="all" //首页显示所有分类
 //var INDEXLISTTYPE ="1" //指定首页显示分类的id
 var PAYTEMPPLATEID = 'hzKpxuPF2rw7O-qTElkeoE0lMwr0O4t9PJkLyt6v8rk';//赞赏消息模版id
 var REPLAYTEMPPLATEID = 'IiAVoBWP34u1uwt801rI_Crgen7Xl2lvAGP67ofJLo8';//回复评论消息模版id
-var ZANIMAGEURL = 'https://www.watch-life.net/images/2017/07/zsm400.jpg';//微信赞赏的图片链接，用于个人小程序的赞赏
+var ZANIMAGEURL = 'http://knighter.cn/wp-content/uploads/2018/04/WechatIMG3-e1524223858230.jpeg';//微信赞赏的图片链接，用于个人小程序的赞赏
 var LOGO = "../../images/logo-icon.png"; // 网站的logo图片
 var POSTERIMGURL ="../../images/logo700.png"; //生成海报如果没有首图，使用此处设置的图片作为海报图片。
 //设置downloadFile合法域名,不带https ,在中括号([])里增加域名，格式：{id=**,domain:'www.**.com'}，用英文逗号分隔。
 //此处设置的域名和小程序与小程序后台设置的downloadFile合法域名要一致。
 var DOWNLOADFILEDOMAIN = [
-    { id: 1, domain: 'www.watch-life.net' },
-    { id: 2, domain: 'watch-life.net' },
-    { id: 3, domain: 'www.qiniu.com' }
+    { id: 1, domain: 'knighter.cn' },
+    { id: 2, domain: 'knighter.cn' },
+    { id: 3, domain: 'knighter.cn' }
 ]
  //首页图标导航
  //参数说明：'name'为名称，'image'为图标路径，'url'为跳转的页面，'redirecttype'为跳转的类型，apppage为本小程序的页面，miniapp为其他微信小程序,webpage为web-view的页面
