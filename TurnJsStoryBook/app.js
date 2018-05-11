15aab706ae8d3bd18ea67bd44f53f62237967e78
yangboz <youngwelle@gmail.com>
2018-05-11 11:43:28 +0800

    CHANGED:make angularjs+jquery workable;

--- TurnJsStoryBook/app.js
+++ TurnJsStoryBook/app.js
@@ -138,37 +138,56 @@ app.controller('MasterCtrl', ['$rootScope', '$state', '$scope', 'masterDetailSta
         $rootScope.$on('$stateChangeSuccess', update);
         masterDetailState.onIsSplitViewChanged(update);
         loadData();
-
         //
-        jQuery('#book').turn({acceleration: true,
-            pages: numberOfPages,
-            elevation: 50,
-            gradients: !jQuery.isTouch,
-            when: {
-                turning: function(e, page, view) {
-
-                    // Gets the range of pages that the book needs right now
-                    var range = jQuery(this).turn('range', page);
-
-                    // Check if each page is within the book
-                    for (page = range[0]; page<=range[1]; page++)
-                        addPage(page, jQuery(this));
-
-                },
-
-                turned: function(e, page) {
-                    $('#page-number').val(page);
-                }
+        // Sample using dynamic pages with turn.js
+        var numberOfPages = 1000;
+        // Adds the pages that the book will need
+        function addPage(page, book) {
+            // 	First check if the page is already in the book
+            if (!book.turn('hasPage', page)) {
+                // Create an element for this page
+                var element = $('<div />', {'class': 'page '+((page%2==0) ? 'odd' : 'even'), 'id': 'page-'+page}).html('<i class="loader"></i>');
+                // If not then add the page
+                book.turn('addPage', element, page);
+                // Let's assum that the data is comming from the server and the request takes 1s.
+                setTimeout(function(){
+                    element.html('<div class="data">Data for page '+page+'</div>');
+                }, 1000);
             }
-        });
+        }
+        //
+        $(window).ready(function() {
+            $('#book').turn({
+                acceleration: true,
+                pages: numberOfPages,
+                elevation: 50,
+                gradients: !$.isTouch,
+                when: {
+                    turning: function (e, page, view) {
 
-        jQuery('#number-pages').html(numberOfPages);
+                        // Gets the range of pages that the book needs right now
+                        var range = $(this).turn('range', page);
 
-        jQuery('#page-number').keydown(function(e){
+                        // Check if each page is within the book
+                        for (page = range[0]; page <= range[1]; page++)
+                            addPage(page, $(this));
 
-            if (e.keyCode==13)
-                jQuery('#book').turn('page', jQuery('#page-number').val());
+                    },
 
+                    turned: function (e, page) {
+                        $('#page-number').val(page);
+                    }
+                }
+            });
+
+            $('#number-pages').html(numberOfPages);
+
+            $('#page-number').keydown(function (e) {
+
+                if (e.keyCode == 13)
+                    $('#book').turn('page', $('#page-number').val());
+
+            });
         });
         //
         function goToDefaultDetail() {
