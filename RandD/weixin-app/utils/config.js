949aeee42c4363beacf7e68fbc59d9620d1a7cdc
yangboz <youngwelle@gmail.com>
2018-06-22 13:52:09 +0800

    ADDED:paper for wechat mini-program wechat pay;clear the towxml folder;

--- RandD/weixin-app/utils/config.js
+++ RandD/weixin-app/utils/config.js
@@ -15,7 +15,7 @@
 //如果wordpress没有安装在网站根目录请加上目录路径,例如："www.watch-life.net/blog"
 var DOMAIN = "knighter.cn";
 var MINAPPTYPE="0";//小程序的类型，如果是企业小程序请填：0 ，如果是个人小程序请填：1
-var WEBSITENAME="骑士的勇敢"; //网站名称
+var WEBSITENAME="cStoryBooks"; //网站名称
 var ABOUTID = 1136; //wordpress网站"页面"的id,注意这个"页面"是wordpress的"页面"，不是"文章"
 var PAGECOUNT='10'; //每页文章数目
 var CATEGORIESID='all'  //专题页显示全部的分类
@@ -39,7 +39,7 @@ var DOWNLOADFILEDOMAIN = [
  //        'appid' 当redirecttype为miniapp时，这个值为其他微信小程序的appid，如果redirecttype为apppage，webpage时，这个值设置为空。
  //         'extraData'当redirecttype为miniapp时，这个值为提交到其他微信小程序的参数，如果redirecttype为apppage，webpage时，这个值设置为空。
 var INDEXNAV = [
-    { id: '1', name: '微店', image: '../../images/shop.png', url: 'pages/shelf/shelf', redirecttype: 'miniapp', appid: 'wx55ea6098e41af5c4', extraData:'' },
+  { id: '1', name: '微店', image: '../../images/shop.png', url: 'pages/shelf/shelf', redirecttype: 'miniapp', appid: 'wx764fa42b23cd341f', extraData:'' },
     { id: '2', name: '排行', image: '../../images/ranking.png', url: '../hot/hot', redirecttype: 'apppage', appid: '', extraData:''},   
     ]
 
