72dd551cfbac78e5100e083e7e9fa224d3f5a69c
yangboz <youngwelle@gmail.com>
2019-02-18 13:34:31 +0800

    FIXED:invalid jackson lib version number,changed image-swiper size for wechat mini app;

--- MVP/0.0.1/wechat-mini-app/constants/api-endpoints.js
+++ MVP/0.0.1/wechat-mini-app/constants/api-endpoints.js
@@ -1,5 +1,5 @@
-// export const API_HOST = "http://knighter.cn:8080/storybook";
-export const API_HOST = "http://localhost:8080/storybook";
+export const API_HOST = "http://knighter.cn:8080/storybook";
+// export const API_HOST = "http://localhost:8080/storybook";
 export const API_WX_USER_SAVE = API_HOST +"/wx/user/save/";
 export const API_WX_USER_LOGIN = API_HOST + "/wx/user/";
 export const API_ACCOUNT_ = API_HOST + "/account/";
